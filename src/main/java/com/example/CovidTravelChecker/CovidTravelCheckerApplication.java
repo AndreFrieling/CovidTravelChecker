8d63ee51bcbdbce1eb0de097878741728bd647e3
LinkKF <linkkay123@gmail.com>
2021-03-08 11:01:11 +0100

    initial commit

---
+++ src/main/java/com/example/CovidTravelChecker/CovidTravelCheckerApplication.java
@@ -0,0 +1,13 @@
+package com.example.CovidTravelChecker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidTravelCheckerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidTravelCheckerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/CovidTravelChecker/CovidTravelCheckerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.CovidTravelChecker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidTravelCheckerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
