6b69eb271301b6162bbdbd45130eaeb85282098e
LinkKF <linkkay123@gmail.com>
2021-03-15 10:28:52 +0100

    removed unused variables and imports

--- src/main/java/com/example/CovidTravelChecker/CovidNumberExtractor.java
+++ src/main/java/com/example/CovidTravelChecker/CovidNumberExtractor.java
@@ -1,27 +1,13 @@
 package com.example.CovidTravelChecker;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.JSONPObject;
-import org.apache.tomcat.util.json.JSONParser;
-import org.apache.tomcat.util.json.ParseException;
 import org.json.JSONObject;
-//import sun.net.www.http.HttpClient;
-
 import java.net.URI;
 import java.net.http.HttpClient;
-
 import java.io.*;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.HashMap;
-import java.util.Map;
 
 public class CovidNumberExtractor {
-    URL url;
-    JSONObject jsonAnswer;
     public JSONObject getNumbers(String country) throws IOException, InterruptedException {
 
         HttpClient client = HttpClient.newHttpClient();
