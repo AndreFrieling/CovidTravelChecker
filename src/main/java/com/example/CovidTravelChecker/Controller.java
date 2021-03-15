f10f384883604356053cd4bc637a92517d2918c4
LinkKF <linkkay123@gmail.com>
2021-03-15 10:26:30 +0100

    edit request

--- src/main/java/com/example/CovidTravelChecker/Controller.java
+++ src/main/java/com/example/CovidTravelChecker/Controller.java
@@ -1,17 +1,21 @@
 package com.example.CovidTravelChecker;
 
+import org.json.JSONException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
+
 @RestController
 public class Controller {
 
-    private CovidStatus covidStatus;
+    private CovidStatus covidStatus = new CovidStatus();
 
-    @GetMapping(value = "", consumes = "application.json", produces = "plain/text")
-    public ResponseEntity<String> sendGet(@org.springframework.web.bind.annotation.RequestBody RequestBody parameters){
-        String answer = covidStatus.getStatus(parameters.getCountries());
+    @GetMapping(value = "", consumes = "application/json", produces = "plain/text")
+    public ResponseEntity<String> sendGet(@org.springframework.web.bind.annotation.RequestBody RequestBody parameters) throws IOException, InterruptedException, JSONException {
+        String[] countries = parameters.getCountries();
+        String answer = covidStatus.getStatus(countries);
         return ResponseEntity.status(HttpStatus.ACCEPTED).body(answer);
     }
 }
